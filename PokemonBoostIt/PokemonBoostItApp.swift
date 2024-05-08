2211f16fb02886428e4389cb6f8dc6bed4e00ea6
Andrii Kvashuk <andremtcp2015a@gmail.com>
2024-05-08 11:51:06 +0100

    Initial commit

---
+++ PokemonBoostIt/PokemonBoostItApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonBoostItApp.swift
+//  PokemonBoostIt
+//
+//  Created by Andrii Kvashuk on 08/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct PokemonBoostItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            PokemonListView()
+        }
+    }
+}
---
+++ PokemonBoostIt/PokemonListView.swift
@@ -0,0 +1,19 @@
+//
+//  PokemonListView.swift
+//  PokemonBoostIt
+//
+//  Created by Andrii Kvashuk on 08/05/2024.
+//
+
+import SwiftUI
+
+
+struct PokemonListView: View {
+    var body: some View {
+        Text("Some text")
+    }
+}
+
+#Preview {
+    PokemonListView()
+}
